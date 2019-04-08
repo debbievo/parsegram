c2e02ae0ae5e917c80a85a8b86063382fe32a057
Debbie Vo <debbienvo@yahoo.com>
2019-04-07 21:49:38 -0700

    Initial Commit

---
+++ parsegramTests/parsegramTests.swift
@@ -0,0 +1,34 @@
+//
+//  parsegramTests.swift
+//  parsegramTests
+//
+//  Created by Debbie Vo on 4/7/19.
+//  Copyright © 2019 debbienvo. All rights reserved.
+//
+
+import XCTest
+@testable import parsegram
+
+class parsegramTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
